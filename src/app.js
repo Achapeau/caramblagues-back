fa7f3fc8dbcac4d49cc22e9f6ab091f5250862ba
Achapeau <127884427+Achapeau@users.noreply.github.com>
2025-05-19 16:44:11 +0200

    feat(cors): install and add cors parameters

--- src/app.js
+++ src/app.js
@@ -2,8 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./config/swaggerOption");
+const cors = require("cors");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "https://Achapeau.github.io",
+  })
+);
 
 app.use(express.json());
 app.use(bodyParser.json());
