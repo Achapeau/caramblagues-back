1b234b60bbfb6bac9fbe3b22906524ab356edf08
Achapeau <127884427+Achapeau@users.noreply.github.com>
2025-05-19 08:44:15 +0200

    docs(swagger): set up Swagger for API documentation and document all joke endpoints

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swaggerOption");
 
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const routes = require("./routes/routes");
 app.use("/caramblagues-back", routes);
